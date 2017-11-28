cfa0220adce1f47f58efd68622195c14b0e0eb65
mouaad <aassou.mouad@gmail.com>
2017-11-28 20:23:31 +0000

    test-name-convention

--- src/test/java/com/immoerp/tests/MainTest.java
+++ src/test/java/com/immoerp/tests/MainTest.java
@@ -2,17 +2,15 @@ import org.junit.*;
 
 public class MainTest{
 
-	
-
+	@Test(expected= IllegalArgumentException.class)
 	public void testSample() {
-		Assert.assertEquals(9, Main.selectMaxOfAnArray(new long[] {7, 8, 9}));
+		Assert.assertEquals("the method doesn't throw an exception ", 9, Main.selectMaxOfAnArray(new long[] {}));
 	}
 
-	@Test(expected= IllegalArgumentException.class)
-	public void testOreder() {
-		Assert.assertEquals("", 9, Main.selectMaxOfAnArray(new long[] {9, 8, 7}));
-		Assert.assertEquals("", 9, Main.selectMaxOfAnArray(new long[] {8, 7, 9}));
-		Assert.assertEquals("", 9, Main.selectMaxOfAnArray(new long[] {8, 9, 7}));
-		Assert.assertEquals("", 9, Main.selectMaxOfAnArray(new long[] {}));
+	@Test
+	public void testSelectMaxOfAnArray() {
+		Assert.assertEquals("the result must be ", 9, Main.selectMaxOfAnArray(new long[] {9, 8, 7}));
+		Assert.assertEquals("the result must be ", 9, Main.selectMaxOfAnArray(new long[] {8, 7, 9}));
+		Assert.assertEquals("the result must be ", 9, Main.selectMaxOfAnArray(new long[] {8, 9, 7}));
 	}
 }
