8643d50ba0a19a84d87b32f9a814c91bf4adf34d
mouaad <aassou.mouad@gmail.com>
2017-11-28 21:36:58 +0000

    commit-with-name-convention

--- src/test/java/com/immoerp/tests/MainTest.java
+++ src/test/java/com/immoerp/tests/MainTest.java
@@ -2,15 +2,23 @@ import org.junit.*;
 
 public class MainTest{
 
-	@Test(expected= IllegalArgumentException.class)
-	public void testSample() {
+	@Test(expected = IllegalArgumentException.class)
+	public void testSelectMaxOfAnArrayWithException() {
 		Assert.assertEquals("the method doesn't throw an exception ", 9, Main.selectMaxOfAnArray(new long[] {}));
 	}
 
 	@Test
 	public void testSelectMaxOfAnArray() {
 		Assert.assertEquals("the result must be ", 9, Main.selectMaxOfAnArray(new long[] {9, 8, 7}));
+	}
+
+	@Test
+	public void testSelectMaxOfAnArraySort() {
 		Assert.assertEquals("the result must be ", 9, Main.selectMaxOfAnArray(new long[] {8, 7, 9}));
+	}
+
+	@Test
+	public void testSelectMaxOfAnArrayDuplication() {
 		Assert.assertEquals("the result must be ", 9, Main.selectMaxOfAnArray(new long[] {8, 9, 7}));
 	}
 }
