d2f73ffea032ce146eda642b19b139364116e7b4
mouaad <aassou.mouad@gmail.com>
2017-11-28 19:33:42 +0000

    commit:withJunit4

--- src/test/java/com/immoerp/tests/MainTest.java
+++ src/test/java/com/immoerp/tests/MainTest.java
@@ -1,18 +1,18 @@
-import junit.framework.*;
+import org.junit.*;
 
-public class MainTest extends TestCase{
+public class MainTest{
 
-	public MainTest(String name) {
-		super(name);
-	}
+	
 
 	public void testSample() {
-		assertEquals(9, Main.selectMaxOfAnArray(new long[] {7, 8, 9}));
+		Assert.assertEquals(9, Main.selectMaxOfAnArray(new long[] {7, 8, 9}));
 	}
 
+	@Test(expected= IllegalArgumentException.class)
 	public void testOreder() {
-		assertEquals(9, Main.selectMaxOfAnArray(new long[] {9, 8, 7}));
-		assertEquals(9, Main.selectMaxOfAnArray(new long[] {8, 7, 9}));
-		assertEquals(9, Main.selectMaxOfAnArray(new long[] {8, 9, 7}));
+		Assert.assertEquals("", 9, Main.selectMaxOfAnArray(new long[] {9, 8, 7}));
+		Assert.assertEquals("", 9, Main.selectMaxOfAnArray(new long[] {8, 7, 9}));
+		Assert.assertEquals("", 9, Main.selectMaxOfAnArray(new long[] {8, 9, 7}));
+		Assert.assertEquals("", 9, Main.selectMaxOfAnArray(new long[] {}));
 	}
 }
